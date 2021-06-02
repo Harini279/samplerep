b70b2bf8c11e90ffa6eb0e989efd20064664211d
Harini279 <85232976+Harini279@users.noreply.github.com>
2021-06-02 18:26:54 +0530

    Add files via upload

---
+++ Discount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Price
+{
+    public class Discount
+    {
+        public void Dprice()
+        {
+            Console.WriteLine("Enter sales price");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter discount price");
+            int j = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(i - j);
+        }
+    }
+}
